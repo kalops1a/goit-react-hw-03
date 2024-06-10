bab21d0a3034438ab95d0acd1790049ff0b8ed68
kalops1a <vadymduchenko@gmail.com>
2024-06-10 17:01:31 +0200

    1

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+
+export const App () => {
+  
+
+
+
+
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
+    </div>
+  );
+}
